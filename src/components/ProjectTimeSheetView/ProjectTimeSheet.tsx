aa1fa4b59f0a1031c7ce384763aa384089f013f4
haiderarshad021 <haiderarshad021@gmail.com>
2025-10-27 23:32:01 +0500

    completed the admin and user module

--- src/components/ProjectTimeSheetView/ProjectTimeSheet.tsx
+++ src/components/ProjectTimeSheetView/ProjectTimeSheet.tsx
@@ -683,7 +683,7 @@ const ProjectTimesheetView: React.FC<ProjectTimesheetViewProps> = ({
                  onClick={confirmDeleteDeliverable}
                  className="px-4 py-2 bg-red-600 text-white rounded-lg hover:bg-red-700 text-sm font-medium" // Adjusted styles
                >
-                 Delete
+                Delete
                </button>
              </div>
            </div>
